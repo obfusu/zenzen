c3839919e7056e8d881dc5379d53d181bdf2e497
Ganesh Sundar C <77ganesh.nitt@gmail.com>
2021-11-18 19:24:56 +0530

    Initial Commit

---
+++ zenzen/zenzenApp.swift
@@ -0,0 +1,17 @@
+//
+//  zenzenApp.swift
+//  zenzen
+//
+//  Created by Ganesh on 18/11/21.
+//
+
+import SwiftUI
+
+@main
+struct zenzenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
